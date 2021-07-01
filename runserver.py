a97e95d435124b6d162ab91926e124af23a77c48
mgkim_developer <mgkim.developer@gmail.com>
2021-07-01 21:07:58 +0900

    2021.07.01_How to runserver

---
+++ runserver.py
@@ -0,0 +1,6 @@
+# if you want runserver,
+# Type "python manage.py runserver" in the terminal.
+print('Mingyo Kim')
+print('mgkim.developer@gmail.com')
+print('Dept of Industrial Management Engineering')
+
